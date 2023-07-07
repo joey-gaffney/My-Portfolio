19ead506215b177117b89048ee311d86f69e0cff
joey gaffney <joeygaffney22@gmail.com>
2023-07-07 11:25:20 -0700

    build ready img

--- data/projectdata.js
+++ data/projectdata.js
@@ -5,14 +5,14 @@ export default [
     stack: ["Next.js", "Tailwindcss"],
     previewlink: "https://next-js-dashboard-peach.vercel.app/",
     codelink: "https://github.com/joey-gaffney/Next.js-Dashboard",
-    imgUrl: "public/assets/nextdash.png",
+    imgUrl: "/assets/nextdash.png",
   },
   {
     title: "Personal Portfolio",
     info: "Responsive UI application using Chart.js. Multiple routes using the Next.js built-in routing feature. Using JavaScript, I added dynamic values to differentiate between orders based on their status.",
     stack: ["Vite", "React", "Tailwindcss"],
     previewlink: "https://joeygaffneyjsdeveloper.netlify.app/",
-    codelink: "",
-    imgUrl: "public/prtfoliosite.png",
+    codelink: "https://github.com/joey-gaffney/My-Portfolio",
+    imgUrl: "/assets/prtfoliosite.png",
   },
 ];
